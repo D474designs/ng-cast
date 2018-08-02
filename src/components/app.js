50cbac5f6ce20bbcda71ae3631cdcd577a1b1a2c
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-08-02 16:25:25 -0500

    add prototype init

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,24 @@
 angular.module('video-player')
-
 .component('app', {
-  // TODO
+
+  controller: function (youTube) {
+    this.selectVideo = (index) => {
+      this.currentVideo = this.videos[index];
+    };
+
+    this.updateVideos = (list) => {
+      this.videos = list;
+      this.currentVideo = this.videos[0];
+    };
+
+    this.searchResults = (input) => {
+      youTube.search(input, this.updateVideos);
+    };
+
+    youTube.search('coffee', this.updateVideos);
+
+    this.currentVideo = this.videos ? this.videos[0] : {};
+  },
+
+  templateUrl: 'src/templates/app.html'
 });
